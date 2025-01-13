a13245da653be781b44eb1e72cd14da0204fee46
Maram <marambargaoui9@gmail.com>
2025-01-13 19:52:17 +0100

    change

--- shows.py
+++ shows.py
@@ -1,13 +1,7 @@
 import pandas as pd
 
-
-# Step 1: Read the CSV file into a DataFrame
 df = pd.read_csv('Shows.csv')  
 
-# Make sure the CSV file is in the same directory or provide the full path
-
-
-# Step 2: Display the DataFrame
 print("Data from CSV:")
 print(df)
 
